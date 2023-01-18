6049748e712a1e3db297ca8551642e411680b0b3
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-18 20:12:46 -0300

    feat(): first commit, base project spring boot web

---
+++ src/main/java/br/com/charleseduardo/apilog/ApilogApplication.java
@@ -0,0 +1,14 @@
+package br.com.charleseduardo.apilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilogApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApilogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/charleseduardo/apilog/ApilogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.charleseduardo.apilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
