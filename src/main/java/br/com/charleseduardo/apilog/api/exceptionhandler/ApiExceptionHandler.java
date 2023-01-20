4107b7833a0a5b8a2321da5ae715bacdebd0c622
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-20 15:48:23 -0300

    feat(): implents Exception Handler

---
+++ src/main/java/br/com/charleseduardo/apilog/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,40 @@
+package br.com.charleseduardo.apilog.api.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        var problem = new Problem();
+        List<Problem.Fields> fields = new ArrayList<>();
+
+        for (ObjectError error: ex.getBindingResult().getAllErrors()){
+
+            String name = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            fields.add(new Problem.Fields(name, message));
+
+        }
+        problem.setStatus(status.value());
+        problem.setTitle("One or more fields are invalid. Fill in correctly and try again.");
+        problem.setDateHour(LocalDateTime.now());
+        problem.setFields(fields);
+
+
+        return  handleExceptionInternal(ex,problem,headers,status, request);
+    }
+}
---
+++ src/main/java/br/com/charleseduardo/apilog/api/exceptionhandler/Problem.java
@@ -0,0 +1,70 @@
+package br.com.charleseduardo.apilog.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Problem {
+
+    private Integer status;
+    private LocalDateTime dateHour;
+    private String title;
+    private List<Fields> fields;
+
+    public static class Fields{
+
+        private String name;
+        private String message;
+
+        public Fields(String name, String message) {
+            this.name = name;
+            this.message = message;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setDateHour(LocalDateTime dateHour) {
+        this.dateHour = dateHour;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setFields(List<Fields> fields) {
+        this.fields = fields;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getDateHour() {
+        return dateHour;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Fields> getFields() {
+        return fields;
+    }
+}
