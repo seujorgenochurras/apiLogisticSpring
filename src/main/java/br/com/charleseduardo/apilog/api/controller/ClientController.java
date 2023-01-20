98680a0afac0ddbf180d1b84abb4c13acac92e37
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-20 14:08:46 -0300

    refactor(ClientController): optimization structure code

--- src/main/java/br/com/charleseduardo/apilog/api/controller/ClientController.java
+++ src/main/java/br/com/charleseduardo/apilog/api/controller/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
     private ClientRepository clientRepository;
@@ -24,19 +26,14 @@ public class ClientController {
         this.clientRepository = clientRepository;
     }
 
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> list(){
         return clientRepository.findAll();
     }
-@GetMapping("/clients/{clientId}")
+@GetMapping("/{clientId}")
     public ResponseEntity<Client> search(@PathVariable Long clientId){
-        Optional<Client> client = clientRepository.findById(clientId);
-
-        if(client.isPresent()){
-
-            return ResponseEntity.ok(client.get());
-        }
-        return ResponseEntity.notFound().build();
-
+       return clientRepository.findById(clientId)
+               .map(client -> ResponseEntity.ok(client))
+               .orElse(ResponseEntity.notFound().build());
     }
 }
